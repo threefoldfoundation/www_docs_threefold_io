d003560f8e9e98b0dd373c76ce9d39f854bc970f
mik-tf <logismos@protonmail.ch>
2024-08-24 09:00:20 -0400

    removed icon in title

--- docs_website/src/pages/index.tsx
+++ docs_website/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Learn and Explore the TFGrid ⏱️
+            Learn and Explore the TFGrid
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Everything About ThreeFold">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
